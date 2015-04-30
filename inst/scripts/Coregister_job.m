efd9c59a77976414b46331d33758d09a2cf5aecc
muschellij2 <muschellij2@gmail.com>
2015-04-30 02:27:38 -0400

    Making Coregister.

---
+++ inst/scripts/Coregister_job.m
@@ -0,0 +1,16 @@
+%-----------------------------------------------------------------------
+% Job saved on 29-Apr-2015 15:42:22 by cfg_util (rev $Rev: 6134 $)
+% spm SPM - SPM12 (6225)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.coreg.estwrite.ref = {'%reffile%'};
+matlabbatch{1}.spm.spatial.coreg.estwrite.source = {'%sourcefile%'};
+matlabbatch{1}.spm.spatial.coreg.estwrite.other = {%otherfile%};
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.cost_fun = 'nmi';
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.sep = [4 2];
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.fwhm = [7 7];
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.interp = 4;
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.mask = 0;
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.prefix = '%prefix%';
