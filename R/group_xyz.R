ee74799f0556e8747e2832fec44a8b56af99eb6e
muschellij2 <muschellij2@gmail.com>
2015-09-01 23:49:49 -0400

    Added group_xyz

---
+++ R/group_xyz.R
@@ -0,0 +1,26 @@
+#' @title Center of Gravity for Multiple Areas
+#' @description Find Center of Gravity of Each Area of Image, after thresholding
+#' @param img Object of class nifti
+#' @return Matrix of 3 columns
+#' @import fslr
+#' @export
+group_xyz = function(img){
+  #   stopifnot(inherits(img, "nifti"))
+  les_xyz = spm_bwlabel(img, 
+                        binary = FALSE, 
+                        retimg = TRUE, 
+                        k = 1, 
+                        ...) 
+  les_levs = sort(unique(les_xyz[les_xyz!=0]))
+  if (length(les_levs) == 1){
+    return(t(xyz(les_xyz)))
+  } else {
+    les_xyz = t(sapply(les_levs, function(x){
+      xyz(les_xyz == x)
+    })) 
+    return(les_xyz)
+  }
+  return(les_xyz)
+}
+
+  
