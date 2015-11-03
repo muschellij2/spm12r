4d6430c916de5c231e66ef784711c50323135826
muschellij2 <muschellij2@gmail.com>
2015-11-03 18:44:17 -0500

    took out spm_dir example

--- R/spm_dir.R
+++ R/spm_dir.R
@@ -3,8 +3,6 @@
 #' @description Returns the SPM12 directory 
 #' @export
 #' @return Chracter vector of spm12 paths
-#' @examples 
-#' spm_dir()
 spm_dir <- function(){
   install_spm12()
   return(system.file("spm12", package="spm12r"))
