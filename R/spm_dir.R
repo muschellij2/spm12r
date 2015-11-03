5a5558681e09792db8e5258b0d53df9bccea55de
muschellij2 <muschellij2@gmail.com>
2015-11-03 18:45:51 -0500

    should succeed in checks

--- R/spm_dir.R
+++ R/spm_dir.R
@@ -5,5 +5,5 @@
 #' @return Chracter vector of spm12 paths
 spm_dir <- function(){
   install_spm12()
-  return(system.file("spm12", package="spm12r"))
+  return(system.file("spm12", package = "spm12r"))
 }
