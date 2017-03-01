3d776aaffd198c8162743d5d5e7608d66671593f
muschellij2 <muschellij2@gmail.com>
2017-03-01 16:20:30 -0500

    Adding some stuff

---
+++ inst/scripts/OldNorm_job.m
@@ -0,0 +1,22 @@
+%-----------------------------------------------------------------------
+% Job saved on 27-Feb-2017 14:26:41 by cfg_util (rev $Rev: 6134 $)
+% spm SPM - SPM12 (6225)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.tools.oldnorm.estwrite.subj.source = {%filename%};
+matlabbatch{1}.spm.tools.oldnorm.estwrite.subj.wtsrc = '';
+matlabbatch{1}.spm.tools.oldnorm.estwrite.subj.resample = %resample%
+matlabbatch{1}.spm.tools.oldnorm.estwrite.eoptions.template = %template%
+matlabbatch{1}.spm.tools.oldnorm.estwrite.eoptions.weight = '';
+matlabbatch{1}.spm.tools.oldnorm.estwrite.eoptions.smosrc = 8;
+matlabbatch{1}.spm.tools.oldnorm.estwrite.eoptions.smoref = 0;
+matlabbatch{1}.spm.tools.oldnorm.estwrite.eoptions.regtype = 'mni';
+matlabbatch{1}.spm.tools.oldnorm.estwrite.eoptions.cutoff = 25;
+matlabbatch{1}.spm.tools.oldnorm.estwrite.eoptions.nits = 16;
+matlabbatch{1}.spm.tools.oldnorm.estwrite.eoptions.reg = 1;
+matlabbatch{1}.spm.tools.oldnorm.estwrite.roptions.preserve = 0;
+matlabbatch{1}.spm.tools.oldnorm.estwrite.roptions.bb = %bbox%;
+matlabbatch{1}.spm.tools.oldnorm.estwrite.roptions.vox = [2 2 2];
+matlabbatch{1}.spm.tools.oldnorm.estwrite.roptions.interp = 1;
+matlabbatch{1}.spm.tools.oldnorm.estwrite.roptions.wrap = [0 0 0];
+matlabbatch{1}.spm.tools.oldnorm.estwrite.roptions.prefix = 'w';
